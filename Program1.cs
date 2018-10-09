cef30c2fae4ef32b1431e63eb1d12655ba55261b
Владислав <vladmainop@gmail.com>
2018-10-09 18:22:46 +0300

    added task1 & hw1

---
+++ Program1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество коэффициентов 1го мнгогочлена");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество коэффициентов 2го мнгогочлена");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            if (n1 > n2)
+            {
+                count = n1;
+            }
+            else
+            {
+                count = n2;
+            }
+
+            double[] polinom1 = new double[count];
+            double[] polinom2 = new double[count];
+
+            Console.WriteLine("Введите коэффициенты 1го мнгогочлена в обратном порядке");
+            for (int i = 0; i < n1;i++)
+            {
+                polinom1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите коэффициенты 2го мнгогочлена в обратном порядке");
+            for (int i = 0; i < n2; i++)
+            {
+                polinom2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            double[] result = CalculatePolinom(polinom1, polinom2);
+
+            Console.WriteLine("Ответ:");
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+       public static double[] CalculatePolinom(double[] polinom1, double[] polinom2)
+        {
+            int count = 0;
+            if(polinom1.Length > polinom2.Length)
+            {
+                count = polinom1.Length;
+            }
+            else
+            {
+                count = polinom2.Length;
+            }
+
+            double[] result = new double[count];
+            for(int i = 0; i < count; i++)
+            {
+                result[i] = polinom1[i] + polinom2[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tr_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine("Type number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] res = GetIntArray(a);
+            for (int i = 1; i <= n; i++)
+            {  
+                int[] worker = ArrayMult(res, i);
+                res = worker;
+            }
+
+            string s = "";
+            bool trigger = false;
+            for(int i = 0; i < res.Length; i++)
+            {
+                if(res[i] == 0 && trigger == false)
+                {
+                    continue;
+                }
+                else
+                {
+                    trigger = true;                    
+                }
+                if(trigger == true)
+                {
+                    s += res[i];
+                }
+            }
+            Console.WriteLine(s);
+        }
+
+        static int[] GetIntArray(int n)
+        {
+            string nConv = Convert.ToString(n);
+
+            int[] Conv = new int[nConv.Length];
+
+            for (int i = 0; i < Conv.Length; i++)
+            {
+                Conv[i] = (int)Char.GetNumericValue(nConv[i]);
+            }
+
+            return Conv;
+        }
+
+        static int[] ArrayMult(int[] arr, int n)
+        {
+            int tmp = 0;
+            int[] helper = new int[100000];
+            int heLen = helper.Length - 1;
+            for(int i = arr.Length -1; i >=0; i--)
+            {
+                helper[heLen] = arr[i];
+                heLen--;
+            }
+
+            for (int i = helper.Length - 1; i >= 0; i--)
+            {
+                int mult = (helper[i] * n) + tmp;
+                tmp = mult / 10;
+                helper[i] = mult % 10;
+            }
+
+            return helper;
+        }
+    }
+}
+
