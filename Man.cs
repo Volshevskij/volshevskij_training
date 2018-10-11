ba05f21106fc9593b6ef6dd12e62bc526ac592ec
Владислав <vladmainop@gmail.com>
2018-10-11 20:59:50 +0300

    added hw_6_1

---
+++ Man.cs
@@ -0,0 +1,58 @@
+﻿namespace EPAM_TK_3
+{
+    class Man
+    {
+        protected string _name;
+        protected int _age;
+        protected string _gender;
+        protected int _weight;
+
+        public  Man(string name, int age, string gender, int weight)
+        {
+            _name = name;
+            _age = age;
+            _gender = gender;
+            _weight = weight;
+        }
+
+        public Man():this("John", 20, "male", 70)
+        {
+
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public void SetWeight(int weight)
+        {
+            _weight = weight;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int SetAge()
+        {
+            return _age;
+        }
+
+        public int GetWeight()
+        {
+           return _weight; 
+        }
+
+        public string GetGender()
+        {
+            return _gender;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿namespace EPAM_TK_3
+{
+    class Student:Man
+    {
+        protected int _aducationYear;
+
+        public Student() : base()
+        {
+
+        }
+
+        public Student(int year) : base()
+        {
+            _aducationYear = year;
+        }
+
+        public void SetAducationYear(int year)
+        {
+            _aducationYear = year;
+        }
+
+        public int GetAducationYear()
+        {
+           return _aducationYear;
+        }
+
+        public void IncAducationYear(int num)
+        {
+            _aducationYear += num;
+        }
+    }
+}
