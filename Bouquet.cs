a1b606190e57668ae4d09e09ac4159180d0fd2a2
Владислав <vladmainop@gmail.com>
2018-10-11 21:02:01 +0300

    added hw_6_2

---
+++ Bouquet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_TK_3
+{
+    class Bouquet
+    {
+        int _cost;
+        List<Flowers> _bouquet;
+
+        Bouquet(List<Flowers> bouquet)
+        {
+            _bouquet = bouquet;
+        }
+
+        Bouquet()
+        {
+
+        }
+
+        public void AddFlower(Flowers f)
+        {
+            _bouquet.Add(f);
+            _cost += f.GetCost();
+        }
+
+        public int GetBouquetCost()
+        {
+            return _cost;
+        }
+
+        public List<Flowers> GetBouquet()
+        {
+            return _bouquet;
+        }
+
+    }
+}
---
+++ Flowers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_TK_3
+{
+    abstract  class Flowers
+    {
+        private string[] _names = { "Rose", "Carnation", "Tulip", "Violet", "Chamomile" };
+        string _name;
+        int _cost;
+        private int[] _costs = {20, 15, 10, 8, 5 };
+
+        public Flowers()
+        {
+            _name = "Rose";
+            _cost = 20;
+        }
+
+        public  Flowers(string name)
+        {
+            for(int i = 0; i < _names.Length; i++)
+            {
+                if(name == _names[i])
+                {
+                    _name = name;
+                    _cost = _costs[i];
+                }
+            }
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetCost()
+        {
+            return _cost;
+        }
+    }
+}
