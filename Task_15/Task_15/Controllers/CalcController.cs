30ec56ce6ef67552aecd6e2671cb6523b456c15e
Владислав <vladmainop@gmail.com>
2018-11-02 00:26:32 +0300

    added hw 15

---
+++ Task_15/Task_15/Controllers/CalcController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Models.Calc calc = new Models.Calc();
+            calc.RandCalc();
+            calc.GetResult();
+            return View(calc);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Calc calc)
+        {
+            calc.GetResult();
+            if(calc.Answer == calc.Result)
+            {
+                calc.Ret = "Correct";
+            }
+            else
+            {
+                calc.Ret = "Uncorrect";
+            }
+            calc.RandCalc();
+            return View(calc);
+        }
+    }
+}
---
+++ Task_15/Task_15/Models/Calc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculator.Models
+{
+    public class Calc
+    {
+        public double Argument1 { get; set; }
+        public double Argument2 { get; set; }
+        public string Operator { get; set; }
+        public double Result { get; set; }
+        public double Answer { get; set; }
+        public string Ret { get; set; }
+        public Calc()
+        {
+            Ret = " ";
+        }
+        public void RandCalc()
+        {
+            Random r = new Random();
+            Argument1 = r.Next(1, 10);
+            Argument2 = r.Next(1, 10);
+            double tmp = r.Next(0, 3);
+            if (tmp == 0)
+            {
+                Operator = "+";
+            }
+
+            if (tmp == 1)
+            {
+                Operator = "-";
+            }
+
+            if (tmp == 2)
+            {
+                Operator = "*";
+            }
+            GetResult();
+        }
+        public void GetResult()
+        {
+            if (Operator == "+")
+            {
+                Result = Argument1 + Argument2;
+            }
+            if (Operator == "-")
+            {
+                Result = Argument1 - Argument2;
+            }
+            if (Operator == "*")
+            {
+                Result = Argument1 * Argument2;
+            }
+        }
+    }
+}
