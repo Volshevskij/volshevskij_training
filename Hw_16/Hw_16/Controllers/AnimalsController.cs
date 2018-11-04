d4b61d556fe8ad35eaf54908bb98ab5ac1a38f99
Владислав <vladmainop@gmail.com>
2018-11-04 21:31:56 +0300

    added hw 16

---
+++ Hw_16/Hw_16/Controllers/AnimalsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hw_16.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        // GET: Animals
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetNextPage(Object obj)
+        {
+            return View("Index2");
+        }
+    }
+}
---
+++ Hw_16/Hw_16/Controllers/CatController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hw_16.Controllers
+{
+    public class CatController : Controller
+    {
+        // GET: Cat
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Object obj = new Object();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult GetNextPage(Object obj)
+        {
+            return View("Index2");
+        }
+
+
+    }
+}
