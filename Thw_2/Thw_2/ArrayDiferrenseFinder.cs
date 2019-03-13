01c7dea8f98cc0927be89000d6cf62397b29246d
Владислав <vladmainop@gmail.com>
2019-03-13 22:52:23 +0300

    Added hw 2

---
+++ Thw_2/Thw_2/ArrayDiferrenseFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thw_2
+{
+    class ArrayDiferrenseFinder
+    {
+        public bool CheckForSquareEquails(int[] a, int[] b)
+        {
+            if (a == null || b == null || a.Length == 0 || b.Length == 0)
+            {
+                return false;
+            }
+            for(int i = 0; i < a.Length; i++)
+            {
+                int numCounter = 0;
+                int squareCounter = 0;
+                int currentNumber = a[i];
+                int currentNumberSquare = Convert.ToInt32(Math.Pow(currentNumber, 2));
+
+                foreach(int c in a)
+                {
+                    if(c == currentNumber)
+                    {
+                        numCounter++;
+                    }
+                }
+
+                foreach(int c in b)
+                {
+                    if(c == currentNumberSquare)
+                    {
+                        squareCounter++;
+                    }
+                }
+
+                if(numCounter != squareCounter)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Thw_2/Thw_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thw_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayDiferrenseFinder ar = new ArrayDiferrenseFinder();
+
+            int[] a = { 1, -2, -2, 4, 5 };
+            int[] b = { 1, 4, 4, 16, 25 };
+
+            Console.WriteLine(ar.CheckForSquareEquails(a, b));
+
+        }
+    }
+}
